4ba71aea543a49832c7f7ef27701dc9a1eb91362
Hunter Browning <0xFF012D@gmail.com>
2024-06-17 16:11:53 -0700

    Update fpjs.js

--- fpjs.js
+++ fpjs.js
@@ -1,3 +1,10 @@
-function changeColor() {
-    name.style.color = "blue";
+// Get the container element
+const billContainer = document.getElementById('bill');
+
+// Function to add "BILL" to the container
+function addBill() {
+    billContainer.append("BILL ");
 }
+
+// Call addBill every 500 milliseconds
+setInterval(addBill, 500);
